d8d6e54d3c3f2f645da2f01a43c965b9e89a6cae
sachin <sachin.aralapura@gmail.com>
2024-10-16 10:09:19 +0530

    aded grid

--- pp.js
+++ pp.js
@@ -6,7 +6,13 @@ function DrawSqure() {
     // Set the fill color to blue
     Context.fillStyle = 'blue';
     // Draw the square
-    Context.fillRect(50, 50, 100, 100);
+    Context.fillRect(0, 0, 100, 100);
+}
+function DrawImage() {
+    Context.imageSmoothingEnabled = false;
+    var image = new Image();
+    image.src = "cat.jpg";
+    Context.drawImage(image, 0, 0);
 }
 function ScaleUp() {
     Context.scale(2, 2);
@@ -30,6 +36,25 @@ function TranslateXY() {
     Context.translate(50, 50);
 }
 function Clear() {
+    Context.reset();
     Context.restore();
-    Context.clearRect(0, 0, Context.canvas.width, Context.canvas.height);
+    // Context.clearRect(0, 0, Context.canvas.width, Context.canvas.height);
+}
+function DrawCoord(event) {
+    Context.globalCompositeOperation = "destination-over";
+    Context.save();
+    Context.fillStyle = 'green';
+    Context.lineWidth = 1.0;
+    for (var i = 0; i < 1000; i += 10) {
+        Context.beginPath();
+        Context.moveTo(i, 0);
+        Context.lineTo(i, 1000);
+        Context.stroke();
+    }
+    for (var j = 0; j < 1000; j += 10) {
+        Context.beginPath();
+        Context.moveTo(0, j);
+        Context.lineTo(1000, j);
+        Context.stroke();
+    }
 }
--- pp.ts
+++ pp.ts
@@ -1,14 +1,27 @@
 var Canvas: HTMLElement | null = document.getElementById("canvas") as HTMLCanvasElement
 var canvas: HTMLCanvasElement = Canvas as HTMLCanvasElement;
 
-var Context: CanvasRenderingContext2D  = canvas.getContext("2d") as CanvasRenderingContext2D
+var Context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D
 Context.save();
+
+
+
 function DrawSqure(): void {
     // Set the fill color to blue
     Context.fillStyle = 'blue';
 
     // Draw the square
-    Context.fillRect(50, 50, 100, 100);
+    Context.fillRect(0, 0, 100, 100);
+
+
+}
+
+function DrawImage() {
+    Context.imageSmoothingEnabled = false;
+    var image: HTMLImageElement = new Image();
+    image.src = "cat.jpg"
+
+    Context.drawImage(image, 0, 0)
 }
 
 function ScaleUp() {
@@ -39,7 +52,29 @@ function TranslateXY() {
 }
 
 function Clear() {
+    Context.reset();
     Context.restore();
-    Context.clearRect(0, 0, Context.canvas.width, Context.canvas.height);
-       
+    // Context.clearRect(0, 0, Context.canvas.width, Context.canvas.height);
+
 }
+
+function DrawCoord(event: MouseEvent) {
+    Context.globalCompositeOperation = "destination-over";
+    Context.save();
+    Context.fillStyle = 'green';
+    Context.lineWidth = 1.0;
+
+    for (let i = 0; i < 1000; i += 10) {
+        Context.beginPath();
+        Context.moveTo(i, 0);
+        Context.lineTo(i, 1000);
+        Context.stroke();
+    }
+    for (let j = 0; j < 1000; j += 10) {
+        Context.beginPath();
+        Context.moveTo(0, j);
+        Context.lineTo(1000, j);
+        Context.stroke();
+    }
+}
+
