2d6191926b117bd864db1fe96474bb54e2606831
sachin <sachin.aralapura@gmail.com>
2024-10-16 18:12:36 +0530

    pp

--- pp.js
+++ pp.js
@@ -1,60 +1,83 @@
 var Canvas = document.getElementById("canvas");
 var canvas = Canvas;
-var Context = canvas.getContext("2d");
-Context.save();
-function DrawSqure() {
+canvas.width = window.innerWidth - 300;
+canvas.height = window.innerHeight - 300;
+var context;
+var scale = 1;
+canvas.addEventListener("wheel", function (event) {
+    event.preventDefault();
+    if (context === undefined) {
+        return;
+    }
+    Scale(Math.min(Math.max(event.deltaY * -0.01, 0.1), 10));
+});
+context = canvas.getContext("2d");
+context.save();
+function DrawSqure(scale) {
+    if (scale === void 0) { scale = 1; }
     // Set the fill color to blue
-    Context.fillStyle = 'blue';
+    context.fillStyle = 'blue';
     // Draw the square
-    Context.fillRect(0, 0, 100, 100);
+    context.fillRect(50, 50, 100, 100);
 }
 function DrawImage() {
-    Context.imageSmoothingEnabled = false;
+    context.imageSmoothingEnabled = false;
     var image = new Image();
+    image.onload = function () {
+        context.drawImage(image, 0, 0);
+    };
     image.src = "cat.jpg";
-    Context.drawImage(image, 0, 0);
 }
 function ScaleUp() {
-    Context.scale(2, 2);
+    // context.scale(2, 2);
+    Scale(1.1);
+}
+function Scale(x) {
+    scale *= x;
+    context.clearRect(0, 0, canvas.width * scale, canvas.height * scale);
+    context.scale(scale, scale);
+    // context.translate(-(canvas.width * scale - canvas.width), - (canvas.height * scale - canvas.height))
+    DrawSqure(scale);
 }
 function ScaleDown() {
-    Context.scale(0.5, 0.5);
+    // context.scale(0.5, 0.5)
+    Scale(0.9);
 }
 function RotateCW() {
-    Context.rotate(10 * (Math.PI / 180));
+    context.rotate(10 * (Math.PI / 180));
 }
 function RotateCCW() {
-    Context.rotate(-10 * (Math.PI / 180));
+    context.rotate(-10 * (Math.PI / 180));
 }
 function TranslateX() {
-    Context.translate(50, 0);
+    context.translate(50, 0);
 }
 function TranslateY() {
-    Context.translate(0, 50);
+    context.translate(0, 50);
 }
 function TranslateXY() {
-    Context.translate(50, 50);
+    context.translate(50, 50);
 }
 function Clear() {
-    Context.reset();
-    Context.restore();
-    // Context.clearRect(0, 0, Context.canvas.width, Context.canvas.height);
+    context.reset();
+    context.restore();
+    // context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 }
 function DrawCoord(event) {
-    Context.globalCompositeOperation = "destination-over";
-    Context.save();
-    Context.fillStyle = 'green';
-    Context.lineWidth = 1.0;
+    context.globalCompositeOperation = "destination-over";
+    context.save();
+    context.fillStyle = 'green';
+    context.lineWidth = 1.0;
     for (var i = 0; i < 1000; i += 10) {
-        Context.beginPath();
-        Context.moveTo(i, 0);
-        Context.lineTo(i, 1000);
-        Context.stroke();
+        context.beginPath();
+        context.moveTo(i, 0);
+        context.lineTo(i, 1000);
+        context.stroke();
     }
     for (var j = 0; j < 1000; j += 10) {
-        Context.beginPath();
-        Context.moveTo(0, j);
-        Context.lineTo(1000, j);
-        Context.stroke();
+        context.beginPath();
+        context.moveTo(0, j);
+        context.lineTo(1000, j);
+        context.stroke();
     }
 }
--- pp.ts
+++ pp.ts
@@ -1,80 +1,78 @@
 var Canvas: HTMLElement | null = document.getElementById("canvas") as HTMLCanvasElement
 var canvas: HTMLCanvasElement = Canvas as HTMLCanvasElement;
-
-var Context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D
-Context.save();
-
-
-
-function DrawSqure(): void {
-    // Set the fill color to blue
-    Context.fillStyle = 'blue';
-
-    // Draw the square
-    Context.fillRect(0, 0, 100, 100);
+canvas.width = window.innerWidth - 300;
+canvas.height = window.innerHeight - 300;
+var context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D
 
 
+
+function DrawSqure(scale: number = 1): void {
+    context.fillStyle = 'blue';
+    context.fillRect(50, 50, 100, 100);
 }
 
 function DrawImage() {
-    Context.imageSmoothingEnabled = false;
+    context.imageSmoothingEnabled = false;
     var image: HTMLImageElement = new Image();
+    image.onload = function () {
+        context.drawImage(image, 0, 0)
+    }
     image.src = "cat.jpg"
 
-    Context.drawImage(image, 0, 0)
 }
 
 function ScaleUp() {
-    Context.scale(2, 2);
+    context.scale(2, 2);
 }
 
 
 function ScaleDown() {
-    Context.scale(0.5, 0.5)
+    context.scale(0.5, 0.5)
 }
 
 function RotateCW() {
-    Context.rotate(10 * (Math.PI / 180))
+    context.rotate(10 * (Math.PI / 180))
 }
 function RotateCCW() {
-    Context.rotate(-10 * (Math.PI / 180))
+    context.rotate(-10 * (Math.PI / 180))
 }
 
 function TranslateX() {
-    Context.translate(50, 0);
+    context.translate(50, 0);
 }
 
 function TranslateY() {
-    Context.translate(0, 50);
+    context.translate(0, 50);
 }
 function TranslateXY() {
-    Context.translate(50, 50);
+    context.translate(50, 50);
 }
 
 function Clear() {
-    Context.reset();
-    Context.restore();
-    // Context.clearRect(0, 0, Context.canvas.width, Context.canvas.height);
+    context.reset();
+    context.restore();
+    // context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
 }
 
 function DrawCoord(event: MouseEvent) {
-    Context.globalCompositeOperation = "destination-over";
-    Context.save();
-    Context.fillStyle = 'green';
-    Context.lineWidth = 1.0;
+
+    context.globalCompositeOperation = "destination-over";
+    context.save();
+    context.fillStyle = 'green';
+    context.lineWidth = 1.0;
 
     for (let i = 0; i < 1000; i += 10) {
-        Context.beginPath();
-        Context.moveTo(i, 0);
-        Context.lineTo(i, 1000);
-        Context.stroke();
+        context.beginPath();
+        context.moveTo(i, 0);
+        context.lineTo(i, 1000);
+        context.stroke();
     }
     for (let j = 0; j < 1000; j += 10) {
-        Context.beginPath();
-        Context.moveTo(0, j);
-        Context.lineTo(1000, j);
-        Context.stroke();
+        context.beginPath();
+        context.moveTo(0, j);
+        context.lineTo(1000, j);
+        context.stroke();
     }
 }
 
